22a716013d0fa45cee41e13265b6d0d942c5776f
root <root@v22016111230740197.happysrv.de>
2017-04-27 16:40:12 +0200

    modified discover.py

--- src/discover.py
+++ src/discover.py
@@ -3,9 +3,14 @@ import os, slackclient
 SLACK_FUN_NAME = os.environ.get('SLACK_FUN_NAME')
 SLACK_FUN_TOKEN = os.environ.get('SLACK_FUN_TOKEN')
 # initialize slack client
-valet_slack_client = slackclient.SlackClient(SLACK_FUN_TOKEN)
+slack_client = slackclient.SlackClient(SLACK_FUN_TOKEN)
 # check if everything is alright
 print(SLACK_FUN_NAME)
 print(SLACK_FUN_TOKEN)
-is_ok = valet_slack_client.api_call("users.list").get('ok')
+is_ok = slack_client.api_call("users.list").get('ok')
+# find the id of our slack bot
+if(is_ok):
+    for user in slack_client.api_call("users.list").get('members'):
+        if user.get('name') == SLACK_FUN_NAME:
+            print(user.get('id'))
 print(is_ok)
